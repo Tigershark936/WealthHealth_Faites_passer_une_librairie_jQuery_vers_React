1156509133f0c99d5660e19e77b4754e92118562
Alain Daly <alain.daly.93600@gmail.com>
2025-09-29 11:04:45 +0200

    feat: add store.js for run redux + persistgate on startup for employee state

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react"
 import App from "./App.jsx"
-import { store } from "./store/store.js"
+import { store, persistor } from "./store/store.js"
 import "./app.css"
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    {/* </PersistGate> */}
+    </PersistGate>
   </Provider>
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,39 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit"
-
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import employees from '../features/employees/employeesSlice'
 import {
   persistStore,
   persistReducer,
-} 
+  // Actions spéciales de redux-persist
+  FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+// Importation du reducer 
+const rootReducer = combineReducers ({
+  employees, 
+})
+
+// configuration de ma persistance
+const persistConfig = {
+  key: 'root', 
+  version: 1,
+  storage, 
+  whitelist: ['employees']
+}
+
+// Création d'un persistReducer afin de se save et de réhydrate 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// Creation du store 
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefault) =>
+    getDefault({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      }
+    }),
+})
+
+// Le persistor qui va piloter la synchro du store vers le localStorage (<PersistGate>)
+export const persistor = persistStore(store)
